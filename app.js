96cf7a96bb4a1a18a6690625026ccafd0e09cba3
SeanCorcoran <sean.corcoran15@gmail.com>
2020-10-14 06:21:22 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { App } = require('@slack/bolt');
+const { sayHello, helloButton } = require("./handlers/hello");
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+// Commands go here
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => sayHello({ message, say }));
+// Responds to the button when clicked
+app.action('button_click', async ({ body, ack, say }) => helloButton({ body, ack, say}));
+
+
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
+
---
+++ handlers/hello/hello.js
@@ -0,0 +1,39 @@
+'use strict';
+
+async function sayHello({ message, say }) {
+  // Basic Hello
+  // await say(`Hey there <@${message.user}>!`); 
+
+  // Fancy Hello
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  });
+}
+
+async function helloButton({body, ack, say}) {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+}
+
+module.exports = {
+  sayHello,
+  helloButton
+}
